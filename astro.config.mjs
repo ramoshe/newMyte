53b2faf47b6c71c32288c3985957bdee9136d5a1
Rachel Sherard <rsherard@mac.com>
2023-12-18 19:45:57 -0500

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import matomo from "@jop-software/astro-matomo";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		matomo({
-			baseUrl: "https://matomo.ramoshe.com/",
-			siteId: 3,
-		}),
-	],
+  integrations: [matomo({
+    baseUrl: "https://matomo.ramoshe.com/",
+    siteId: 3
+  })],
+  output: "server",
+  adapter: netlify()
 });
