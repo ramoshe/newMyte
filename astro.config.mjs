2725ce0bcce951a485c6c351a9d2daf05af16188
Rachel Sherard <rsherard@mac.com>
2023-12-18 22:22:12 -0500

    get major structure and styles in place

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-import matomo from "@jop-software/astro-matomo";
+// TODO uncomment to initiate Matomo
+// import matomo from "@jop-software/astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [matomo({
-    baseUrl: "https://matomo.ramoshe.com/",
-    siteId: 3
-  })],
+  integrations: [
+    // matomo({
+    //   baseUrl: "https://matomo.ramoshe.com/",
+    //   siteId: 3
+    // })
+  ],
 });
