7b824d34ee35101e316d439cbe647c1d0485bdc2
Rachel Sherard <rsherard@mac.com>
2023-12-18 19:47:53 -0500

    remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import matomo from "@jop-software/astro-matomo";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [matomo({
     baseUrl: "https://matomo.ramoshe.com/",
     siteId: 3
   })],
-  output: "server",
-  adapter: netlify()
 });
