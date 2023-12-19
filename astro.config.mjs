04566c646ee882b489e5839665fec3dccf350874
Rachel Sherard <rsherard@mac.com>
2023-12-18 19:30:20 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+import matomo from "@jop-software/astro-matomo";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		matomo({
+			baseUrl: "https://matomo.ramoshe.com/",
+			siteId: 3,
+		}),
+	],
+});
