6f7054d65aaf852267be63aec63875d93c15d6cd
Rachel Sherard <rsherard@mac.com>
2023-12-27 01:17:32 -0500

    add sitemap and robots for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 // TODO uncomment to initiate Matomo
 // import matomo from "@jop-software/astro-matomo";
 
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // matomo({
-    //   baseUrl: "https://matomo.ramoshe.com/",
-    //   siteId: 3
-    // })
-  ],
+  site: 'https://new.myte.site',
+  integrations: [robotsTxt(), sitemap()]
+  // matomo({
+  //   baseUrl: "https://matomo.ramoshe.com/",
+  //   siteId: 3
+  // })
 });
