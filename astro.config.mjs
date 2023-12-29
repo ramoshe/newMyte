fd6e88c29b6f8d5b0eb70ea7f148f3e089e9963d
Rachel Sherard <rsherard@mac.com>
2023-12-29 02:26:36 -0500

    change matomo integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig } from 'astro/config';
-import matomo from "@jop-software/astro-matomo";
-
 import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
+import matomo from "astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://new.myte.site',
-  integrations: [
-    robotsTxt(),
-    sitemap(),
-    matomo({
-      baseUrl: "https://matomo.ramoshe.com/",
-      siteId: 3
-    }),
-  ],
+	site: "https://new.myte.site",
+	integrations: [
+		robotsTxt(),
+		sitemap(),
+		matomo({
+			enabled: import.meta.env.PROD, // Only load in production
+			host: "https://matomo.ramoshe.com/",
+			// trackerUrl: "js/", // defaults to matomo.php
+			// srcUrl: "js/", // defaults to matomo.js
+			siteId: 3,
+		}),
+	],
 });
